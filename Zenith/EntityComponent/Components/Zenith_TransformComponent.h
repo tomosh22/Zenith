6f60ced66f31f747123ee45154ee7447a6f5d959
tomosh22 <tomosh22@googlemail.com>
2024-07-21 20:58:14 +0100

    added a second sphere and removed redundant Transform class

--- Zenith/EntityComponent/Components/Zenith_TransformComponent.h
+++ Zenith/EntityComponent/Components/Zenith_TransformComponent.h
@@ -5,31 +5,6 @@ namespace reactphysics3d {
 	class RigidBody;
 }
 
-class Transform
-{
-public:
-	Transform() {};
-	Transform(Zenith_Maths::Vector3 xPos, Zenith_Maths::Vector3 xScale, Zenith_Maths::Quat xRot /*= glm::quat_identity<float, glm::defaultp>()*/) : m_xPosition(xPos), m_xScale(xScale), m_xRotationQuat(xRot) {}
-
-	void SetPosition(const Zenith_Maths::Vector3& pos);
-	void SetRotationQuat(const Zenith_Maths::Quat& rot);
-	void SetScale(const Zenith_Maths::Vector3& xScale);
-	inline void UpdateMatrix();
-	void UpdateRotation();
-	Zenith_Maths::Matrix4 m_xMatrix /*= glm::identity<float>()*/;
-
-	Zenith_Maths::Quat m_xRotationQuat /*= glm::quat_identity<float, glm::packed_highp>()*/;
-	float m_fRoll = 0, m_fYaw = 0, m_fPitch = 0;
-	Zenith_Maths::Vector3 m_xPosition = { 0,0,0 };
-	Zenith_Maths::Vector3 m_xScale = { 1,1,1 };
-
-	static Zenith_Maths::Matrix4 RotationMatFromQuat(const Zenith_Maths::Quat& quat);
-	static Zenith_Maths::Matrix4 RotationMatFromVec3(float degrees, const Zenith_Maths::Vector3& axis);
-	static Zenith_Maths::Quat EulerAnglesToQuat(float roll, float yaw, float pitch);
-private:
-	float m_fPrevRoll = 0, m_fPrevYaw = 0, m_fPrevPitch = 0;
-};
-
 class Zenith_TransformComponent
 {
 public:
