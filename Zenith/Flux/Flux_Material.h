d1f8ad5e3ca45492c0608b9ea68931d5a7b5affb
tomosh22 <tomosh22@googlemail.com>
2024-07-22 22:04:41 +0100

    fixed naming in Flux_Material

--- Zenith/Flux/Flux_Material.h
+++ Zenith/Flux/Flux_Material.h
@@ -6,35 +6,35 @@ class Flux_Material
 {
 public:
 	Flux_Material()
-		: m_xDiffuse(&Flux_Graphics::s_xBlankTexture2D)
-		, m_xNormal(&Flux_Graphics::s_xBlankTexture2D)
-		, m_xRoughness(&Flux_Graphics::s_xBlankTexture2D)
-		, m_xMetallic(&Flux_Graphics::s_xBlankTexture2D)
+		: m_pxDiffuse(&Flux_Graphics::s_xBlankTexture2D)
+		, m_pxNormal(&Flux_Graphics::s_xBlankTexture2D)
+		, m_pxRoughness(&Flux_Graphics::s_xBlankTexture2D)
+		, m_pxMetallic(&Flux_Graphics::s_xBlankTexture2D)
 	{
 
 	}
-	Flux_Material(Flux_Texture* xDiffuse, Flux_Texture* xNormal, Flux_Texture* xRoughness, Flux_Texture* xMetallic)
-		: m_xDiffuse(xDiffuse)
-		, m_xNormal(xNormal)
-		, m_xRoughness(xRoughness)
-		, m_xMetallic(xMetallic)
+	Flux_Material(Flux_Texture* pxDiffuse, Flux_Texture* pxNormal, Flux_Texture* pxRoughness, Flux_Texture* pxMetallic)
+		: m_pxDiffuse(pxDiffuse)
+		, m_pxNormal(pxNormal)
+		, m_pxRoughness(pxRoughness)
+		, m_pxMetallic(pxMetallic)
 	{
 
 	}
 
-	void SetDiffuse(Flux_Texture* xDiffuse) { m_xDiffuse = xDiffuse; }
-	void SetNormal(Flux_Texture* xNormal) { m_xNormal = xNormal; }
-	void SetRoughness(Flux_Texture* xRoughness) { m_xRoughness = xRoughness; }
-	void SetMetallic(Flux_Texture* xMetallic) { m_xMetallic = xMetallic; }
+	void SetDiffuse(Flux_Texture* pxDiffuse) { m_pxDiffuse = pxDiffuse; }
+	void SetNormal(Flux_Texture* pxNormal) { m_pxNormal = pxNormal; }
+	void SetRoughness(Flux_Texture* pxRoughness) { m_pxRoughness = pxRoughness; }
+	void SetMetallic(Flux_Texture* pxMetallic) { m_pxMetallic = pxMetallic; }
 
-	Flux_Texture* GetDiffuse() { return m_xDiffuse; }
-	Flux_Texture* GetNormal() { return m_xNormal; }
-	Flux_Texture* GetRoughness() { return m_xRoughness; }
-	Flux_Texture* GetMetallic() { return m_xMetallic; }
+	Flux_Texture* GetDiffuse() { return m_pxDiffuse; }
+	Flux_Texture* GetNormal() { return m_pxNormal; }
+	Flux_Texture* GetRoughness() { return m_pxRoughness; }
+	Flux_Texture* GetMetallic() { return m_pxMetallic; }
 
 private:
-	Flux_Texture* m_xDiffuse;
-	Flux_Texture* m_xNormal;
-	Flux_Texture* m_xRoughness;
-	Flux_Texture* m_xMetallic;
+	Flux_Texture* m_pxDiffuse;
+	Flux_Texture* m_pxNormal;
+	Flux_Texture* m_pxRoughness;
+	Flux_Texture* m_pxMetallic;
 };
