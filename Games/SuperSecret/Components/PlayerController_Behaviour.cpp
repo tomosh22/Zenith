ac4df59acad62982488c5010ed12667686146bbe
Thomas O'Shaughnessy <tomosh22@googlemail.com>
2025-04-18 21:21:13 +0100

    perspective camera, character movement and reading/writing map data file

--- Games/SuperSecret/Components/PlayerController_Behaviour.cpp
+++ Games/SuperSecret/Components/PlayerController_Behaviour.cpp
@@ -8,8 +8,6 @@
 #include "DebugVariables/Zenith_DebugVariables.h"
 #include "EntityComponent/Zenith_Entity.h"
 
-DEBUGVAR float dbg_fCamDistance = 25.f;
-
 static Zenith_Entity s_axBulletEntities[128];
 static u_int s_uCurrentBulletIndex = 0;
 
@@ -17,32 +15,25 @@ PlayerController_Behaviour::PlayerController_Behaviour(Zenith_Entity& xParentEnt
 	: m_xParentEntity(xParentEntity)
 	, m_xPosition({0,0})
 {
-#ifdef ZENITH_DEBUG_VARIABLES
-	static bool ls_bInit = false;
-	if (!ls_bInit)
-	{
-		ls_bInit = true;
-		Zenith_DebugVariables::AddFloat({ "PlayerController", "Camera Distance" }, dbg_fCamDistance, 0, 50);
-	}
-#endif
+
 }
 
 
 void PlayerController_Behaviour::OnUpdate(const float fDt)
 {
-	if (Zenith_Input::WasKeyPressedThisFrame(ZENITH_KEY_W))
+	if (Zenith_Input::WasKeyPressedThisFrame(ZENITH_KEY_UP))
 	{
 		m_xPosition.y++;
 	}
-	if (Zenith_Input::WasKeyPressedThisFrame(ZENITH_KEY_S))
+	if (Zenith_Input::WasKeyPressedThisFrame(ZENITH_KEY_DOWN))
 	{
 		m_xPosition.y--;
 	}
-	if (Zenith_Input::WasKeyPressedThisFrame(ZENITH_KEY_A))
+	if (Zenith_Input::WasKeyPressedThisFrame(ZENITH_KEY_LEFT))
 	{
 		m_xPosition.x--;
 	}
-	if (Zenith_Input::WasKeyPressedThisFrame(ZENITH_KEY_D))
+	if (Zenith_Input::WasKeyPressedThisFrame(ZENITH_KEY_RIGHT))
 	{
 		m_xPosition.x++;
 	}
--- Games/SuperSecret/SuperSecret_State_InGame.cpp
+++ Games/SuperSecret/SuperSecret_State_InGame.cpp
@@ -8,11 +8,14 @@
 #include "Flux/Flux_Graphics.h"
 #include "Flux/Quads/Flux_Quads.h"
 #include "Input/Zenith_Input.h"
+#include "Components/CameraController_Behaviour.h"
 #include "Components/PlayerController_Behaviour.h"
+#include "DebugVariables/Zenith_DebugVariables.h"
+#include "FileAccess/Zenith_FileAccess.h"
 
-#define QUAD_SIZE 32
-#define MAP_WIDTH 10
-#define MAP_HEIGHT 10
+#define QUAD_SIZE 256
+static uint32_t g_uMapWidth = -1;
+static uint32_t g_uMapHeight = -1;
 
 Zenith_State* Zenith_StateMachine::s_pxCurrentState = new SuperSecret_State_InGame;
 
@@ -21,7 +24,7 @@ static Zenith_Entity s_xPlayer0;
 
 static Flux_Texture* g_pxTextures[SUPERSECRET_TEXTURE_INDEX__COUNT];
 
-static SUPERSECRET_TEXTURE_INDICES s_aeMap[MAP_WIDTH * MAP_HEIGHT];
+static SUPERSECRET_TEXTURE_INDICES* s_aeMap = nullptr;
 
 static void LoadAssets()
 {
@@ -34,12 +37,12 @@ static void LoadAssets()
 
 void UploadQuad(const uint32_t uX, const uint32_t uY, const SUPERSECRET_TEXTURE_INDICES eTexture, const float fSizeMult = 1.0f)
 {
-	Zenith_Assert(uX < MAP_WIDTH && uY < MAP_HEIGHT, "Won't fit on map");
+	Zenith_Assert(uX < g_uMapWidth && uY < g_uMapHeight, "Won't fit on map");
 
 	Flux_Quads::UploadQuad({
 		{
 			(QUAD_SIZE + uX * QUAD_SIZE * 2),
-			(QUAD_SIZE + uY * QUAD_SIZE * 2),
+			(QUAD_SIZE + uY * QUAD_SIZE * 2) + (fSizeMult * QUAD_SIZE),
 			QUAD_SIZE * fSizeMult,
 			QUAD_SIZE * fSizeMult
 		},
@@ -63,34 +66,67 @@ void SuperSecret_State_InGame::OnEnter()
 	const float fFOV = 45;
 	const float fNear = 1;
 	const float fFar = 5000;
-	const float fAspectRatio = 16. / 9.;
+	const float fAspectRatio = 256. / 192.;
 	xCamera.InitialisePerspective(xPos, fPitch, fYaw, fFOV, fNear, fFar, fAspectRatio);
+	xCamera.SetPosition({40, 30, -60});
 	xScene.SetMainCameraEntity(s_xController);
+	s_xController.AddComponent<Zenith_ScriptComponent>().SetBehaviour<CameraController_Behaviour>();
+
 
 	s_xPlayer0.Initialise(&xScene, "Player0");
-	Zenith_ScriptComponent& xScript = s_xPlayer0.AddComponent<Zenith_ScriptComponent>();
-	xScript.SetBehaviour<PlayerController_Behaviour>();
+	s_xPlayer0.AddComponent<Zenith_ScriptComponent>().SetBehaviour<PlayerController_Behaviour>();
 
+	char* pcData = Zenith_FileAccess::ReadFile(ASSETS_ROOT"Maps/map0.txt");
+	uint32_t ulCursor = 0;
+	
+	g_uMapWidth = atoi(pcData + ulCursor);
+	ulCursor += std::to_string(g_uMapWidth).length() + 1;
+	g_uMapHeight = atoi(pcData + ulCursor);
+	ulCursor += std::to_string(g_uMapHeight).length() + 1;
 
-	for (uint32_t u = 0; u < MAP_WIDTH * MAP_HEIGHT; u++)
+	s_aeMap = new SUPERSECRET_TEXTURE_INDICES[g_uMapHeight * g_uMapHeight];
+	for (uint32_t u = 0; u < g_uMapWidth * g_uMapHeight; u++)
 	{
-		s_aeMap[u] = SUPERSECRET_TEXTURE_INDEX__LONG_GRASS;
+		uint32_t uRead = atoi(pcData + ulCursor);
+		ulCursor += std::to_string(uRead).length() + 1;
+
+		s_aeMap[u] = static_cast<SUPERSECRET_TEXTURE_INDICES>(uRead);
+		Zenith_DebugVariables::AddUInt32({ "Map",std::to_string(u) }, s_aeMap[u], 0, SUPERSECRET_TEXTURE_INDEX__COUNT - 1);
 	}
 }
 
 void SuperSecret_State_InGame::OnUpdate()
 {
-	for (uint32_t u = 0; u < MAP_WIDTH * MAP_HEIGHT; u++)
+	for (uint32_t u = 0; u < g_uMapWidth * g_uMapHeight; u++)
 	{
-		UploadQuad(u % MAP_WIDTH, u / MAP_WIDTH, s_aeMap[u]);
+		UploadQuad(u % g_uMapWidth, u / g_uMapWidth, static_cast<SUPERSECRET_TEXTURE_INDICES>(s_aeMap[u]));
 	}
 	const PlayerController_Behaviour& xPlayer = *(PlayerController_Behaviour*)s_xPlayer0.GetComponent<Zenith_ScriptComponent>().m_pxScriptBehaviour;
 	UploadQuad(xPlayer.GetPosition().x, xPlayer.GetPosition().y, SUPERSECRET_TEXTURE_INDEX__PLAYER0, 2.0f);
+
+	if (Zenith_Input::WasKeyPressedThisFrame(ZENITH_KEY_P))
+	{
+		FILE* pxFile = fopen(ASSETS_ROOT"Maps/map0.txt", "wb");
+
+		fputs(std::to_string(g_uMapWidth).c_str(), pxFile);
+		fputs("\n", pxFile);
+		fputs(std::to_string(g_uMapHeight).c_str(), pxFile);
+		fputs("\n", pxFile);
+
+		for (uint32_t u = 0; u < g_uMapWidth * g_uMapHeight; u++)
+		{
+			fputs(std::to_string(s_aeMap[u]).c_str(), pxFile);
+			fputs("\n", pxFile);
+		}
+
+		fclose(pxFile);
+	}
 
 	Zenith_Core::Zenith_MainLoop();
 }
 
 void SuperSecret_State_InGame::OnExit()
 {
+	delete[] s_aeMap;
 	Zenith_Scene::GetCurrentScene().Reset();
 }
