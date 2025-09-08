13c2c7f32694a86b56aaf438e509192dbe16a69c
tomosh22 <tomosh22@googlemail.com>
2025-09-08 12:08:43 +0100

    Fixed FluxCompiler build error

--- Zenith/Core/Memory/Zenith_MemoryManagement_Enabled.h
+++ Zenith/Core/Memory/Zenith_MemoryManagement_Enabled.h
@@ -1,4 +1,6 @@
+#ifdef ZENITH_MEMORY_MANAGEMENT_ENABLED
 #define new new(__LINE__, __FILE__)
 #define malloc(x) static_assert(false, "malloc forbidden when memory profiled");
 #define realloc(x, y) static_assert(false, "realloc forbidden when memory profiled");
-#define free(x) static_assert(false, "free forbidden when memory profiled"); 
+#define free(x) static_assert(false, "free forbidden when memory profiled");
+#endif
